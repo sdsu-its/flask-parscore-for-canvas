e495ed816b71ce6bc74f5f9625177ee25da6a887
Carter Andrews <andrewscart@gmail.com>
2019-11-01 09:11:08 +0000

    many to many database structure

--- app/models.py
+++ app/models.py
@@ -63,4 +63,29 @@ class Course(db.Model):
     def __repr__(self):
         return '<Course {}>'.format(self.course_name)
 
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
+association_table = Table('association', Base.metadata,
+    Column('left_id', Integer, ForeignKey('left.id')),
+    Column('right_id', Integer, ForeignKey('right.id'))
+)
+
+class Parent(Base):
+    __tablename__ = 'left'
+    id = Column(Integer, primary_key=True)
+    children = relationship(
+        "Child",
+        secondary=association_table,
+        back_populates="parents")
+
+class Child(Base):
+    __tablename__ = 'right'
+    id = Column(Integer, primary_key=True)
+    parents = relationship(
+        "Parent",
+        secondary=association_table,
+        back_populates="children")
