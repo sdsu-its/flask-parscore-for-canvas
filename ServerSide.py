4b33a745adc533fc3c06f7975894ece9927f6dd6
Carter Andrews <andrewscart@gmail.com>
2020-06-05 16:31:46 +0000

    refactored

--- ServerSide.py
+++ ServerSide.py
@@ -1,11 +1,10 @@
 
 #imports
-from app import app, db
-from app.models import User, Admin, Teacher, Major, Student, Course
+from app import app
 
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Course': Course, 'Teacher':Teacher,'Student':Student,'Major':Major,'Admin':Admin}
+#@app.shell_context_processor
+#def make_shell_context():
+#    return {'db': db, 'User': User, 'Course': Course, 'Teacher':Teacher,'Student':Student,'Major':Major,'Admin':Admin}
 if __name__ == '__main__':
     app.run()
 
--- app/routes.py
+++ app/routes.py
@@ -1,74 +1,41 @@
 from app import app
-from flask import Flask, render_template , flash ,redirect, url_for
-from app.forms import LoginForm
-from flask_login import current_user, login_user
-from app.models import User, Course, Major, Admin, Student, Teacher
-from flask_login import logout_user
-from flask_login import login_required
-#Routes
-#---Each function represents a different page given by the app.route argument
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('index'))
-    return render_template('login.html', title='Sign In', form=form)
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-@app.route('/Users')
-def Users():
-    UserList=User.query.all()
-    TeacherList=User.query.filter(User.access_level=='Teacher').all()
-    AdminList=User.query.filter(User.access_level=='Admin').all()
-    StudentList=User.query.filter(User.access_level=='Student').all()
-    return render_template('Users.html',title='Users',UserList=UserList,AdminList=AdminList,StudentList=StudentList,TeacherList=TeacherList)
-@app.route('/classes')
-def classes():
-    current_courses =Course.query.all()
-    return render_template('classes.html',title='Classes',course_list=current_courses)
-@app.route('/majors')
-def majors():
-    current_majors = Major.query.all()
-    return render_template('majors.html',title='Majors',major_list=current_majors)
-@app.route('/')
-@app.route('/index')
-@login_required
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+
+DOWNLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__)) + '/uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+from app import pandasParscoreParser
+
+def process_file(path, filename):
+   pandasParscoreParser.parscoreParser(path)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    print("going to home screen")
+    if request.method == 'POST':
+       if 'file' not in request.files:
+           print('No file attached in request')
+           return redirect(request.url)
+       file = request.files['file']
+       if file.filename == '':
+           print('No file selected')
+           return redirect(request.url)
+       if file and allowed_file(file.filename):
+           filename = secure_filename(file.filename)
+           file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+           process_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), filename)
+           return redirect(url_for('uploaded_file', filename=filename))
     return render_template('index.html')
 
-from app import db
-from app.forms import RegistrationForm
 
-# ...
-@login_required
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if not current_user.access_level == 'Admin':
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user=User()
-        print('the radio button said')
-        print(form.access_level.data)
-        if(form.access_level.data=='student'):
-            user = Student(username=form.username.data, email=form.email.data)
-        if(form.access_level.data=='system'):
-            user = Admin(username=form.username.data, email=form.email.data)
-        if(form.access_level.data=='teacher'):
-            user = Teacher(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you have registered a new User')
-        return redirect(url_for('index'))
-    return render_template('register.html', title='Register', form=form)
+
+ALLOWED_EXTENSIONS = {'csv'}
+def allowed_file(filename):
+   return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
