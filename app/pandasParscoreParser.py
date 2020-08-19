c27de4a29591d5f9c894660ef92e43111f6a89c1
Ubuntu <ubuntu@ip-172-31-70-145.ec2.internal>
2020-08-19 22:24:03 +0000

    fixing upload to webportal

--- app/pandasParscoreParser.py
+++ app/pandasParscoreParser.py
@@ -2,14 +2,14 @@ import sys
 import os
 import pandas as pd
 import datetime
-
+import csv
 currDate = datetime.datetime.now()
 
 #run program
 def parscoreParser(inputFile):
     newData = readFile(inputFile)
     #changed this line to put changes back into file rather than printing them
-    inputConv(newData).to_csv(index=False,path_or_buf=inputFile)
+    inputConv(newData).to_csv(index=False,path_or_buf=inputFile, quoting=csv.QUOTE_ALL)
 
 
 #read in file from upser input
@@ -35,34 +35,34 @@ def inputConv(dataFrame):
         convDf = dataFrame[['SIS User ID','Last Name','First Name','Override Score']]
         convDf.rename(columns = {"SIS User ID": "Username",
                                   "Override Score":"Course Letter Grade"})
-        convDf.insert(3, "Last Access Date", currDate.strftime("%Y-%m-%d %H:%M:%S"), True)
-        retDf = convDf.reindex(columns=("Last Name","First Name","SIS User ID","Last Access Date","Override Score"))
+        convDf.insert(3, "Last Access", currDate.strftime("%Y-%m-%d %H:%M:%S"), True)
+        retDf = convDf.reindex(columns=("Last Name","First Name","SIS User ID","Last Access","Override Score"))
         retDf = retDf.rename(columns = {"SIS User ID": "Username","Override Score":"Course Letter Grade"})
         retDf["Course Letter Grade"]=detectMissing(retDf["Course Letter Grade"],dataFrame["Final Score"])
         retDf = retDf.apply(lambda x: seriesIteration(x) if x.name == "Course Letter Grade" else x)
     elif 'Override Grade' in dataFrame:
         convDf = dataFrame[['SIS User ID','Last Name','First Name','Override Grade']]
         convDf.rename(columns = {"SIS User ID": "Username",'Override Grade':'Course Letter Grade'})
-        convDf.insert(3, "Last Access Date", currDate.strftime("%Y-%m-%d %H:%M:%S"), True)
-        retDf = convDf.reindex(columns=("Last Name","First Name","SIS User ID","Last Access Date","Override Grade"))
+        convDf.insert(3, "Last Access", currDate.strftime("%Y-%m-%d %H:%M:%S"), True)
+        retDf = convDf.reindex(columns=("Last Name","First Name","SIS User ID","Last Access","Override Grade"))
         retDf = retDf.rename(columns = {"SIS User ID": "Username","Override Grade":"Course Letter Grade"})
         retDf["Course Letter Grade"]=detectMissing(retDf["Course Letter Grade"],dataFrame["Final Grade"])
     elif 'Final Grade' in dataFrame:
         convDf = dataFrame[['SIS User ID','Last Name','First Name','Final Grade']]
         convDf.rename(columns = {"SIS User ID": "Username",'Final Grade':'Course Letter Grade'})
-        convDf.insert(3, "Last Access Date", currDate.strftime("%Y-%m-%d %H:%M:%S"), True)
-        retDf = convDf.reindex(columns=("Last Name","First Name","SIS User ID","Last Access Date","Final Grade"))
+        convDf.insert(3, "Last Access", currDate.strftime("%Y-%m-%d %H:%M:%S"), True)
+        retDf = convDf.reindex(columns=("Last Name","First Name","SIS User ID","Last Access","Final Grade"))
         retDf = retDf.rename(columns = {"SIS User ID": "Username","Final Grade":"Course Letter Grade"})
     elif  'Final Score' in dataFrame:
     	convDf = dataFrame[['SIS User ID','Last Name','First Name','Final Score']]
     	convDf.rename(columns = {"SIS User ID": "Username",'Final Score':'Course Letter Grade'})
-    	convDf.insert(3, "Last Access Date", currDate.strftime("%Y-%m-%d %H:%M:%S"), True)
-    	retDf = convDf.reindex(columns=("Last Name","First Name","SIS User ID","Last Access Date","Final Score"))
-    	retDf = retDf.rename(columns = {"SIS User ID": "RED ID","Final Score":"Course Letter Grade"})
+    	convDf.insert(3, "Last Access", currDate.strftime("%Y-%m-%d %H:%M:%S"), True)
+    	retDf = convDf.reindex(columns=("Last Name","First Name","SIS User ID","Last Access","Final Score"))
+    	retDf = retDf.rename(columns = {"SIS User ID": "Username","Final Score":"Course Letter Grade"})
     	retDf = retDf.apply(lambda x: seriesIteration(x) if x.name == "Course Letter Grade" else x)
     modDfObj = retDf.apply(lambda x: stripSeries(x) if x.name == "First Name" else x)
     modDfObj = modDfObj.apply(lambda x: stripSeries(x) if x.name == "Last Name" else x)
-
+    modDfObj["Username"] = modDfObj["Username"].astype(int)
     return modDfObj
 
 def detectMissing(overrideSeries,baseSeries):
--- flaskapp.py
+++ flaskapp.py
@@ -1,10 +1,6 @@
 
-#imports
 from app import app
 
-#@app.shell_context_processor
-#def make_shell_context():
-#    return {'db': db, 'User': User, 'Course': Course, 'Teacher':Teacher,'Student':Student,'Major':Major,'Admin':Admin}
 import os
 from elevate import elevate
 
